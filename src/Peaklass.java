8cd0ac22bb1868f6e657e666b53ca53b54d88b90
OliverAlbertHallik <oliveralbert.hallik@gmail.com>
2026-04-01 18:32:58 +0300

    Lisatud peaklassi käivitamisel kuvatav programmi tutvustav lühiinfo

--- src/Peaklass.java
+++ src/Peaklass.java
@@ -2,6 +2,9 @@ import java.util.Random;
 
 public class Peaklass {
     public static void main(String[] args) {
+        System.out.println("Bussipiletite ostmine: Müügil on piletid bussis vabadele kohtadele.");
+        System.out.println("Pärast ostetavate piletite soovitud koguse sisestamist pakutakse juhuslikult valitud piletid vabadele kohtadele, kasutaja saab tehingu kinnitada või ostmisest loobuda.");
+        System.out.println("Kui piletid on müüdud, on edaspidi bussis vähem vabu kohti järel. Ülejäänud piletite ostmist saab jätkata, kuni küsitakse korra rohkem pileteid, kui vabu kohti järel on.\n");
         Random juhuslik = new Random();
         int pileteidKokku = juhuslik.nextInt(30,151);
         Müük müük = new Müük(pileteidKokku); // Loob piletite müügi, milles alguses on etteantud juhuslik arv pileteid.
