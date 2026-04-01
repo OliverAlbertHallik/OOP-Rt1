05961d4c5cbf0d0a58144b8cd600a1fca58b7573
OliverAlbertHallik <oliveralbert.hallik@gmail.com>
2026-04-01 18:17:07 +0300

    Täiendatud müügi ja peaklassi, parandatud klassis Müük saadaval piletite nimekirjast koopia tegemine.

--- src/Müük.java
+++ src/Müük.java
@@ -16,14 +16,33 @@ public class Müük {
     public boolean ostuKatse(int n) {
         if (n > piletidMüügil.size())
             return false; // Annab peaklassi meetodile teada, et programmi töö tuleb lõpetada.
+        if (n == 0) {
+            System.out.println("Pileteid ei soovitud osta.");
+            return true;
+        }
+        if (n < 0) {
+            System.out.println("Siin ei saa ostetud pileteid tagastada.");
+            return true;
+        }
 
-        List<Pilet> ümberJärjestatav = new ArrayList<>(); // Nii säilib müügil olevate piletite korrastatud kasvav järjekord.
-        Collections.copy(ümberJärjestatav,piletidMüügil);
+        List<Pilet> ümberJärjestatav = new ArrayList<>(); // Nii säilib müügil olevate piletite korrastatud kasvav järjekord, lisaks võimaldab uuest ArrayList-ist juba valitud piletid eemaldada.
+        ümberJärjestatav.addAll(piletidMüügil);
         List<Pilet> pakutavad = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
-
+            Collections.shuffle(ümberJärjestatav); // Juhusliku valimiseks juhuslik järjekord.
+            pakutavad.add(ümberJärjestatav.getFirst()); // Valida pilet, mis on juhuslikult sattunud esimeseks.
+            ümberJärjestatav.removeFirst(); // Eemaldada vastav pilet sellest loetelust, et sama piletit ei pakutaks mitmekordselt.
         }
+        System.out.print("\nOstmiseks pakutavad piletid:\n[");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n-1; i++) {
+            sb.append(pakutavad.get(i).getKoht());
+            sb.append(',');
+        }
+        sb.append(pakutavad.getLast().getKoht());
+        sb.append(']');
+        System.out.println(sb);
         return true;
     }
 
--- src/Peaklass.java
+++ src/Peaklass.java
@@ -3,9 +3,14 @@ import java.util.Random;
 public class Peaklass {
     public static void main(String[] args) {
         Random juhuslik = new Random();
-        int pileteidKokku = juhuslik.nextInt(30,150);
-        Müük müük = new Müük(pileteidKokku);
+        int pileteidKokku = juhuslik.nextInt(30,151);
+        Müük müük = new Müük(pileteidKokku); // Loob piletite müügi, milles alguses on etteantud juhuslik arv pileteid.
         System.out.println("Pileteid on kokku " + pileteidKokku);
         //System.out.println(müük.getPiletidMüügil());
+        int ostusoov = 0;
+        do {
+
+        } while (müük.ostuKatse(ostusoov));
+        System.out.println("Küsiti rohkem pileteid, kui vabu kohti jäänud on.");
     }
 }
